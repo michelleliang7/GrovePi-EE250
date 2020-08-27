c8d9cc5f625d378110ddd48d08c351b8c98f694c
michelleliang7 <maliang@usc.edu>
2020-08-27 09:24:34 -0700

    Test LCD

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -32,52 +32,54 @@ from grove_rgb_lcd import *
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
 be true"""
 if __name__ == '__main__':
-	PORT = 4    # D4
-	full_angle = 300
-	adc_ref = 5
-	grove_vcc = 5
-	potentiometer = 0
-	sensor_old = -1
-	dist_old = -1
-	dist = sensor_value = -1
-	grovepi.pinMode(potentiometer,"INPUT")
-	while True:
-		#So we do not poll the sensors too quickly which may introduce noise,
-		#sleep for a reasonable time of 200ms between each iteration.
-		time.sleep(0.2)
+	setText('hi')
+	setRGB(0,120,0)
+	# PORT = 4    # D4
+	# full_angle = 300
+	# adc_ref = 5
+	# grove_vcc = 5
+	# potentiometer = 0
+	# sensor_old = -1
+	# dist_old = -1
+	# dist = sensor_value = -1
+	# grovepi.pinMode(potentiometer,"INPUT")
+	# while True:
+	# 	#So we do not poll the sensors too quickly which may introduce noise,
+	# 	#sleep for a reasonable time of 200ms between each iteration.
+	# 	time.sleep(0.2)
 
-		#print(grovepi.ultrasonicRead(PORT))
-		# Read distance value from Ultrasonic
-		try:
-		# Read distance value from Ultrasonic
-			dist = grovepi.ultrasonicRead(PORT)
-	except TypeError:
-		print ("Error")
-	except IOError:
-		print ("Error")
+	# 	#print(grovepi.ultrasonicRead(PORT))
+	# 	# Read distance value from Ultrasonic
+	# 	try:
+	# 	# Read distance value from Ultrasonic
+	# 		dist = grovepi.ultrasonicRead(PORT)
+	# except TypeError:
+	# 	print ("Error")
+	# except IOError:
+	# 	print ("Error")
 
-		# Read sensor value from potentiometer
-	try:
-		sensor_value = grovepi.analogRead(potentiometer)
-	except KeyboardInterrupt:
-		break
-	except IOError:
-		print ("Error")
+	# 	# Read sensor value from potentiometer
+	# try:
+	# 	sensor_value = grovepi.analogRead(potentiometer)
+	# except KeyboardInterrupt:
+	# 	break
+	# except IOError:
+	# 	print ("Error")
 
-	# Calculate voltage
-	voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
+	# # Calculate voltage
+	# voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
 
-	# Calculate rotation in degrees (0 to 300)
-	degrees = round((voltage * full_angle) / grove_vcc, 2)
+	# # Calculate rotation in degrees (0 to 300)
+	# degrees = round((voltage * full_angle) / grove_vcc, 2)
 
-	# Calculate threshold distance determined by potentiometer
-	thresh = 517/300*degrees
+	# # Calculate threshold distance determined by potentiometer
+	# thresh = 517/300*degrees
 
-	if sensor_value != sensor_old or dist != dist_old:
-		if dist<thresh: 
-			setRGB(255,0,0) # set backlight to red
-			setText(thresh, "cm OBJ PRES")
-		else: 
-			setRGB(0,255,0) # set backlight to green 
-			setText(thresh, "cm")
-		setText_norefresh("\n",dist,"cm")
+	# if sensor_value != sensor_old or dist != dist_old:
+	# 	if dist<thresh: 
+	# 		setRGB(255,0,0) # set backlight to red
+	# 		setText(thresh, "cm OBJ PRES")
+	# 	else: 
+	# 		setRGB(0,255,0) # set backlight to green 
+	# 		setText(thresh, "cm")
+	# 	setText_norefresh("\n",dist,"cm")
