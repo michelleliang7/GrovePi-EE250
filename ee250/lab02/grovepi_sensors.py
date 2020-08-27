079a8d419b9744e9180f2e25b85f9ddede86e016
michelleliang7 <maliang@usc.edu>
2020-08-27 09:30:07 -0700

    Fixed test indentation

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -51,33 +51,33 @@ if __name__ == '__main__':
 		try:
 		# Read distance value from Ultrasonic
 			dist = grovepi.ultrasonicRead(PORT)
-	except TypeError:
-		print ("Error")
-	except IOError:
-		print ("Error")
+		except TypeError:
+			print ("Error")
+		except IOError:
+			print ("Error")
 
-		# Read sensor value from potentiometer
-	try:
-		sensor_value = grovepi.analogRead(potentiometer)
-	except KeyboardInterrupt:
-		break
-	except IOError:
-		print ("Error")
+			# Read sensor value from potentiometer
+		try:
+			sensor_value = grovepi.analogRead(potentiometer)
+		except KeyboardInterrupt:
+			break
+		except IOError:
+			print ("Error")
 
-	# Calculate voltage
-	voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
+		# Calculate voltage
+		voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
 
-	# Calculate rotation in degrees (0 to 300)
-	degrees = round((voltage * full_angle) / grove_vcc, 2)
+		# Calculate rotation in degrees (0 to 300)
+		degrees = round((voltage * full_angle) / grove_vcc, 2)
 
-	# Calculate threshold distance determined by potentiometer
-	thresh = 517/300*degrees
+		# Calculate threshold distance determined by potentiometer
+		thresh = 517/300*degrees
 
-	if sensor_value != sensor_old or dist != dist_old:
-		if dist<thresh: 
-			setRGB(255,0,0) # set backlight to red
-			setText(thresh, "cm OBJ PRES")
-		else: 
-			setRGB(0,255,0) # set backlight to green 
-			setText(thresh, "cm")
-		setText_norefresh("\n",dist,"cm")
+		if sensor_value != sensor_old or dist != dist_old:
+			if dist<thresh: 
+				setRGB(255,0,0) # set backlight to red
+				setText(thresh, "cm OBJ PRES")
+			else: 
+				setRGB(0,255,0) # set backlight to green 
+				setText(thresh, "cm")
+			setText_norefresh("\n",dist,"cm")
